d1a96c6b76c73651547957267506939220ff0ca6
Dana Spiegel <dana@sociabledesign.com>
2014-09-30 17:43:20 -0400

    correctly handled errors

--- registration/views.py
+++ registration/views.py
@@ -4,6 +4,7 @@ from django.conf import settings
 import django.http
 import django.views.generic.edit
 import django.core.urlresolvers
+import django.contrib.messages
 
 import fiftythree.client
 
@@ -23,10 +24,24 @@ class StateLookupView(django.views.generic.edit.FormView):
             settings.FIFTYTHREE_CLIENT_ENDPOINT)
         postal_code = form.cleaned_data['postal_code']
         email = form.cleaned_data['email']
+
         try:
             c.submit_email(email, postal_code)
+        except fiftythree.client.InvalidDataError as e:
+            if e.message == 'Invalid email.':
+                form.add_error('email', e.message)
+            else:
+                form.add_error('postal_code', e.message)
+            django.contrib.messages.error(self.request, e.message)
+            return super(StateLookupView, self).form_invalid(form)
+
+        try:
             r = c.lookup_postal_code(postal_code)
-        except fiftythree.client.InvalidDataError:
+        except fiftythree.client.InvalidDataError as e:
+            django.contrib.messages.error(self.request, e.message)
+            for field, errors in e.errors.items():
+                for error in errors:
+                    form.add_error(field, error)
             return super(StateLookupView, self).form_invalid(form)
         if r and not r['accepts_registration']:
             return django.http.HttpResponseRedirect(r['redirect_url'])
@@ -48,7 +63,11 @@ class RegisterView(django.views.generic.edit.FormView):
         d = form.cleaned_data
         try:
             r = c.register(**d)
-        except fiftythree.client.InvalidDataError:
+        except fiftythree.client.InvalidDataError as e:
+            django.contrib.messages.error(self.request, e.message)
+            for field, errors in e.errors.items():
+                for error in errors:
+                    form.add_error(field, error)
             return super(RegisterView, self).form_invalid(form)
         return super(RegisterView, self).form_valid(form)
 
