fe041e4f1f280b885be710a3d1d108dee48dd903
Dana Spiegel <dana@sociabledesign.com>
2015-01-21 22:12:17 -0500

    finished cleaning up css for radio buttons and checkboxes

--- registration/bootstrap3_renderers.py
+++ registration/bootstrap3_renderers.py
@@ -1,10 +1,18 @@
-import bootstrap3.renderers
+import re
 
 from django.template.loader import get_template
 from django.template import Context
+from django.forms import (
+    CheckboxInput, ClearableFileInput, RadioSelect, CheckboxSelectMultiple
+)
+from django.forms.extras import SelectDateWidget
 from django.utils.html import strip_tags
 
+import bootstrap3.renderers
 from bootstrap3.forms import render_tag, render_label
+
+
+RE_INPUT_TAG = re.compile(r'(<label.*>)(<input.*/>)')
 
 
 class FiftyThreeFieldRenderer(bootstrap3.renderers.FieldRenderer):
@@ -31,7 +39,26 @@ class FiftyThreeFieldRenderer(bootstrap3.renderers.FieldRenderer):
             html = render_tag('i', attrs=attrs) + html
         return html
 
+    def post_widget_render(self, html):
+        if isinstance(self.widget, RadioSelect):
+            html = self.list_to_class(html, 'radio')
+            html = self.invert_radio_input(html)
+        elif isinstance(self.widget, CheckboxSelectMultiple):
+            html = self.list_to_class(html, 'checkbox')
+        elif isinstance(self.widget, SelectDateWidget):
+            html = self.fix_date_select_input(html)
+        elif isinstance(self.widget, ClearableFileInput):
+            html = self.fix_clearable_file_input(html)
+        elif isinstance(self.widget, CheckboxInput):
+            html = self.put_inside_label(html)
+        return html
+
     def put_inside_label(self, html):
         return html + render_label(
             content=self.field.label, label_for=self.field.id_for_label,
             label_title=strip_tags(self.field_help))
+
+    def invert_radio_input(self, html):
+        # NOTE: THIS IS A KLUDGE!
+        # find the input tag
+        return re.sub(RE_INPUT_TAG, r'\2\1', html)
