c0753b656c118b274dae74781cecc65caa8b0554
Dana Spiegel <dana@sociabledesign.com>
2015-01-24 01:39:27 -0500

    fixed many bugs

--- registration/urls.py
+++ registration/urls.py
@@ -8,11 +8,11 @@ import views
 
 urlpatterns = django.conf.urls.patterns(
     '',
+    # django.conf.urls.url(
+    #     r'^$', django.views.generic.TemplateView.as_view(
+    #         template_name='registration/home.html'), name='home'),
     django.conf.urls.url(
-        r'^$', django.views.generic.TemplateView.as_view(
-            template_name='registration/home.html'), name='home'),
-    django.conf.urls.url(
-        r'^start/$', views.StateLookupView.as_view(), name='start'),
+        r'^$', views.StateLookupView.as_view(), name='start'),
     django.conf.urls.url(
         r'^terms-of-service/$', views.TermsOfServiceView.as_view(),
         name='terms_of_service'),
