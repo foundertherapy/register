1c9eab045d7c4dbf731f7ffc0dd1f3bc5421d7ad
Dana Spiegel <dana@sociabledesign.com>
2015-01-27 03:30:43 -0500

    made cookies secure

--- settings/local.py
+++ settings/local.py
@@ -15,4 +15,7 @@ SECRET_KEY = '+kmou3aat2g72#5m1&jm8)&%e8+ccthb@@x8d359dj_k072azb'
 
 FIFTYTHREE_CLIENT_USE_SECURE = False
 
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
+
 print(u'Running with DEBUG={0}'.format(DEBUG))
