9966925b8aa0a0c29388fa48bc0f95b8a9cc5703
Waseem Omar <waseem-omar@Waseems-MacBook-Pro.local>
2015-10-19 14:20:20 +0300

    #338 Added code for organize widget

--- settings/base.py
+++ settings/base.py
@@ -109,7 +109,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 TEMPLATE_CONTEXT_PROCESSORS = (
---
+++ static/js/widget.js
@@ -0,0 +1,72 @@
+function loadOrganizeIframe() {
+    var iframeTag = document.createElement('iframe');
+    iframeTag.setAttribute('id', 'organize_iframe');
+    iframeTag.setAttribute('src', 'http://127.0.0.1:8888/?company_id=1');
+    iframeTag.setAttribute('class', 'organize-iframe');
+    iframeTag.setAttribute('width', '100%');
+    iframeTag.setAttribute('height', '100%');
+
+    document.getElementById('organize_modal_content').appendChild(iframeTag);
+    document.getElementById('organize_modal').setAttribute('class', 'organize-modal organize-modal-opened');
+}
+
+function closeOrganizeIframe() {
+    document.getElementById('organize_modal').setAttribute('class', 'organize-modal');
+    document.getElementById('organize_modal_content').removeChild(document.getElementById('organize_iframe'));
+}
+
+(function() {
+
+    var linkTag = document.createElement('link');
+    linkTag.setAttribute('rel', 'stylesheet');
+    linkTag.setAttribute('href', 'http://127.0.0.1:8888/static/css/modal.css');
+    linkTag.setAttribute('type', 'text/css');
+    document.getElementsByTagName('head')[0].appendChild(linkTag);
+
+
+    var organizeModalDivTag = document.createElement('div');
+    organizeModalDivTag.setAttribute('id', 'organize_modal');
+    organizeModalDivTag.setAttribute('class', 'organize-modal');
+
+
+    var organizeModalContentDivTag = document.createElement('div');
+    organizeModalContentDivTag.setAttribute('class', 'organize-modal-content');
+    organizeModalContentDivTag.setAttribute('id', 'organize_modal_content');
+
+    var organizeModalCloseDivTag = document.createElement('div');
+    organizeModalCloseDivTag.setAttribute('onclick', 'javascript: closeOrganizeIframe()');
+    organizeModalCloseDivTag.setAttribute('class', 'organize-close');
+    organizeModalCloseDivTag.innerHTML = 'X';
+
+    var iframeTag = document.createElement('iframe');
+    iframeTag.setAttribute('src', 'http://127.0.0.1:8888/');
+    iframeTag.setAttribute('width', '100%');
+    iframeTag.setAttribute('height', '100%');
+    iframeTag.setAttribute('class', 'organize-iframe');
+    iframeTag.setAttribute('id', 'organize_iframe');
+
+    organizeModalContentDivTag.appendChild(organizeModalCloseDivTag);
+    organizeModalDivTag.appendChild(organizeModalContentDivTag);
+
+    document.getElementsByTagName('body')[0].appendChild(organizeModalDivTag);
+
+    var organizeRegistrationDivTag = document.createElement('div');
+    organizeRegistrationDivTag.setAttribute('id', 'organize_registration_div');
+    organizeRegistrationDivTag.setAttribute('class', 'organize-registration-div');
+
+    var organizeRegistrationBtnTag = document.createElement('div');
+    organizeRegistrationBtnTag.setAttribute('onclick', 'javascript: loadOrganizeIframe();');
+    organizeRegistrationBtnTag.setAttribute('class', 'organize-registration-btn');
+    organizeRegistrationBtnTag.innerHTML = 'Donate Now';
+
+    organizeRegistrationDivTag.appendChild(organizeRegistrationBtnTag);
+
+    organizeScriptNode = document.getElementById('organize_widget_script');
+    parentNode = organizeScriptNode.parentNode;
+
+    parentNode.insertBefore(organizeRegistrationDivTag, organizeScriptNode);
+
+})();
+
+
+
