6dfa6b8bb10d28b8cd1e0697d4ce236b6bedeec8
nasief <nasief304@gmail.com>
2016-08-07 18:30:47 +0300

    Added password validator

--- settings/base.py
+++ settings/base.py
@@ -205,6 +205,21 @@ INSTALLED_APPS = (
     'secure_redis',
 )
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    }, {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 9,
+        }
+    }, {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    }, {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 RAVEN_CONFIG = {
     'dsn': os.environ.get('RAVEN_DSN'),
 }
