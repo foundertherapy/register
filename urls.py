3157f08183534be20936fa1b63e1361d2faf026b
nasief <nasief304@gmail.com>
2016-09-18 16:58:59 +0300

    Remove accounts URL

--- urls.py
+++ urls.py
@@ -12,7 +12,6 @@ import django.contrib.admin
 
 urlpatterns = django.conf.urls.patterns(
     '',
-    django.conf.urls.url(r'^', django.conf.urls.include('accounts.urls')),
     django.conf.urls.url(r'^i18n/', django.conf.urls.include('django.conf.urls.i18n')),
     django.conf.urls.url(r'^robots.txt$', django.views.generic.TemplateView.as_view(template_name='robots.txt')),
     django.conf.urls.url(r'^', django.conf.urls.include('registration.urls')),
